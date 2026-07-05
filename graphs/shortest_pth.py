8b996fa3348e283b89a69219cf577527e9cc2492
wm05572 <waled.mahaya@stud.th-deg.de>
2026-07-05 22:50:48 +0200

    fixed logic

--- graphs/shortest_pth.py
+++ graphs/shortest_pth.py
@@ -2,13 +2,15 @@ def shortest_path(start,successors,is_goal):
     frontier=[]
     if is_goal(start):
         return (start)
-    frontier.append(successors(start))
+    frontier.append([[start, successor] for successor in successors(start)])
     while frontier:
         x = frontier.pop(0)
         z = x[-1]
         if is_goal(z):
             return x
-        else:
-            frontier.append(successors(z))
+        for successor in successors(z):
+            y=x
+            y.append(successor)
+            frontier.append(y)
 
         
