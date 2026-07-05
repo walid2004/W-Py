75abedc1d1e6dcc68959a0356e6bcfd4d48d3db0
wm05572 <waled.mahaya@stud.th-deg.de>
2026-07-05 23:07:30 +0200

    fixed flawed code

--- graphs/shortest_pth.py
+++ graphs/shortest_pth.py
@@ -1,16 +1,14 @@
 def shortest_path(start,successors,is_goal):
-    frontier=[]
     if is_goal(start):
-        return (start)
-    frontier.append([[start, successor] for successor in successors(start)])
+        return ([start])
+    frontier= [[start, successor] for successor in successors(start)]
     while frontier:
-        x = frontier.pop(0)
-        z = x[-1]
-        if is_goal(z):
-            return x
-        for successor in successors(z):
-            y=x
-            y.append(successor)
-            frontier.append(y)
+        current_path = frontier.pop(0)
+        current_node = current_path[-1]
+        if is_goal(current_node):
+            return current_path
+        for successor in successors(current_node):
+            new_path = current_path + [successor]
+            frontier.append(new_path)
 
         
