58566ff7464f0ce2a0d8445bd2e9f0bfd2a7ee1a
wm05572 <waled.mahaya@stud.th-deg.de>
2026-07-06 02:41:50 +0200

    edits

--- graphs/glass.py
+++ graphs/glass.py
@@ -12,19 +12,30 @@ class Glass:
     def fulling(self):
         self.level= self.capacity
     
+    def free(self):
+        return (self.capacity - self.level)
+    
     def transferring(self,other_glass):
-        self.level = other_glass.capacity - other_glass.level
+        if other_glass.free()>= self.level:
+            other_glass.level +=self.level
+            self.level = 0
+        else:
+            self.level-= other_glass.free()
+            other_glass.level = other_glass.capacity
         
 
 
 
 def glass_problem(cap_1,cap_2,start_1,start_2, goal_1,goal_2):
+    glass_a = Glass(cap_1)
+    glass_b = Glass(cap_2)
     n=0
+    frontier=[]
     while (start_1 and start_2)!=(goal_1,goal_2):
-
+        
         pass
 
 
 
 
-    return n
+    return len(frontier)
