f51ad7d9e887168d9327b610acc20ad482ba321e
wm05572 <waled.mahaya@stud.th-deg.de>
2026-07-23 18:42:17 +0200

    full denominations functionality

--- algs/coin_graph.py
+++ algs/coin_graph.py
@@ -1,13 +1,13 @@
 #Returns the least number of coins needed to reach a number
 #Applying and utilizing Dynamic Programming Principles
 zz=None
-coins =[5,10,20,50,100,200,500]
-pairs={'solution':set([]), 'xx':{}}
-def coin_to(num,cz=0):
+coins =[1,2,5,10,20,50,100,200,500]
+pairs={'solution':[], 'xx':{}}
+
+def coin_to(num,flag= True):
     global zz
     if num in coins:
         x=1
-        pairs['solution'].add(num)
         print((1,num,0,num))
         pairs['xx'][num]=(1,num,0,num)
         return x
@@ -15,15 +15,23 @@ def coin_to(num,cz=0):
         x = pairs[num]
         return x
     else:
-        z=[(coin_to(num-coin),num,num-coin,coin) for coin in coins if coin <num ]
+        z=[(coin_to(num-coin,flag=False),num,num-coin,coin) for coin in coins if coin <num ]
         y=min(reversed(z), key = lambda item: item[0]) #y = min(reversed(z), key=lambda item: item[0])
         x= 1+min(z)[0]
         zz=y
         pairs['xx'][zz[1]]=zz
         print(zz)
         pairs[num]=x
+        if flag:
+            return x, pair_parser(num)
         return x
-def pair_parser():
     
-    return
-print (coin_to(75))
+def pair_parser(num):
+    xl = pairs['xx']
+    while num >0:
+        focus = xl[num]
+        pairs['solution'].append(focus[3])
+        num-=focus[3]
+    return pairs['solution']
+input =67
+print (coin_to(input))
