4239e8e7589d9ded927300c7df2e762dfc53d96d
wm05572 <waled.mahaya@stud.th-deg.de>
2026-07-23 18:25:26 +0200

    progress regarding the coin counter for each denomination

--- algs/coin_graph.py
+++ algs/coin_graph.py
@@ -2,12 +2,14 @@
 #Applying and utilizing Dynamic Programming Principles
 zz=None
 coins =[5,10,20,50,100,200,500]
-pairs={'solution':set([])}
+pairs={'solution':set([]), 'xx':{}}
 def coin_to(num,cz=0):
     global zz
     if num in coins:
         x=1
         pairs['solution'].add(num)
+        print((1,num,0,num))
+        pairs['xx'][num]=(1,num,0,num)
         return x
     elif num in pairs.keys():
         x = pairs[num]
@@ -17,8 +19,11 @@ def coin_to(num,cz=0):
         y=min(reversed(z), key = lambda item: item[0]) #y = min(reversed(z), key=lambda item: item[0])
         x= 1+min(z)[0]
         zz=y
+        pairs['xx'][zz[1]]=zz
         print(zz)
         pairs[num]=x
         return x
-
+def pair_parser():
+    
+    return
 print (coin_to(75))
